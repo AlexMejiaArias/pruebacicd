613f35a620ba775cffbc3f9c658ec769152de94c
MarySamasat <mariana.cuadrado@samasatsa.com>
2023-06-26 12:15:56 -0500

    prueba 2

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h1>Agregando título</h1>
+      <h1>Agregando título 2, no dañar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
